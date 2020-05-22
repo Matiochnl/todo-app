c99d463a35ee143c15433c993cc402f357347c85
Mateusz Ochel <Mateusz.ohel@gmail.com>
2020-05-22 13:36:02 +0200

    Add class InfoControler in package controller

---
+++ src/main/java/pl/app/todoapp/controller/InfoController.java
@@ -0,0 +1,26 @@
+package pl.app.todoapp.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class InfoController {
+    @Autowired
+    private DataSourceProperties dataSource;
+    @Value(("${task.allowMultipleTasksFromTemplate}"))
+    private String myProp;
+
+    @GetMapping("/info/url")
+    String url() {
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    String myProp(){
+        return myProp;
+    }
+}
