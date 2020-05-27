9acda0650720ddff23dd747b16987929c7547283
Mateusz Ochel <Mateusz.ohel@gmail.com>
2020-05-27 23:27:05 +0200

    Add interface SqlTaskRepository in adapter package

---
+++ src/main/java/pl/app/todoapp/adapter/SqlTaskRepository.java
@@ -0,0 +1,18 @@
+package pl.app.todoapp.adapter;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pl.app.todoapp.model.Task;
+import pl.app.todoapp.model.TaskRepository;
+
+@Repository
+interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+    @Override
+    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    boolean existsById(@Param("id") Integer id);
+
+    @Override
+    boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);
+}
