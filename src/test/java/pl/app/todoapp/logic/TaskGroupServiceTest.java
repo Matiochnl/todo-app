4b281e9f25620ac430f6df090a8c92b404e0ba43
Mateusz Ochel <Mateusz.ohel@gmail.com>
2020-06-02 23:35:49 +0200

    Add new toggleGroup_worksAsExpected test

--- src/test/java/pl/app/todoapp/logic/TaskGroupServiceTest.java
+++ src/test/java/pl/app/todoapp/logic/TaskGroupServiceTest.java
@@ -2,6 +2,7 @@ package pl.app.todoapp.logic;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pl.app.todoapp.model.TaskGroup;
 import pl.app.todoapp.model.TaskGroupRepository;
 import pl.app.todoapp.model.TaskRepository;
 
@@ -17,17 +18,18 @@ class TaskGroupServiceTest {
     @Test
     @DisplayName("should throw when undone tasks")
     void toggleGroup_undoneTasks_throwsIllegalStateException() {
-        var mockTaskRepository = mock(TaskRepository.class);
-        when(mockTaskRepository.existsByDoneIsFalseAndGroup_Id(anyInt())).thenReturn(true);
-
+        // given
+        TaskRepository mockTaskRepository = taskRepositoryReturning(true);
+        // system under test
         var toTest = new TaskGroupService(null, mockTaskRepository);
 
-        var exception = catchThrowable(()-> toTest.toggleGroup(1));
+        // when
+        var exception = catchThrowable(() -> toTest.toggleGroup(1));
 
+        // then
         assertThat(exception)
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("undone tasks");
-
     }
 
     @Test
@@ -49,6 +51,29 @@ class TaskGroupServiceTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("id not found");
     }
+
+    @Test
+    @DisplayName("should toggle group")
+    void toggleGroup_worksAsExpected() {
+        // given
+        TaskRepository mockTaskRepository = taskRepositoryReturning(false);
+        // and
+        var group = new TaskGroup();
+        var beforeToggle = group.isDone();
+        // and
+        var mockRepository = mock(TaskGroupRepository.class);
+        when(mockRepository.findById(anyInt())).thenReturn(Optional.of(group));
+        // system under test
+        var toTest = new TaskGroupService(mockRepository, mockTaskRepository);
+
+        // when
+        toTest.toggleGroup(0);
+
+        // then
+        assertThat(group.isDone()).isEqualTo(!beforeToggle);
+    }
+
+
     private TaskRepository taskRepositoryReturning(final boolean result) {
         var mockTaskRepository = mock(TaskRepository.class);
         when(mockTaskRepository.existsByDoneIsFalseAndGroup_Id(anyInt())).thenReturn(result);
