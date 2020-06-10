d9e8dbe0e742e33fe72df713d215cee835b59fa6
Mateusz Ochel <Mateusz.ohel@gmail.com>
2020-06-10 20:55:47 +0200

    Add TaskControllerIntegrationTest

---
+++ src/test/java/pl/app/todoapp/controller/TaskControllerIntegrationTest.java
@@ -0,0 +1,38 @@
+package pl.app.todoapp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.app.todoapp.model.Task;
+import pl.app.todoapp.model.TaskRepository;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+class TaskControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaskRepository repo;
+
+    @Test
+    void httpGet_returnsGivenTask() throws Exception {
+        int id = repo.save(new Task("foo", LocalDateTime.now())).getId();
+
+        mockMvc.perform(get("/tasks/"+ id))
+                .andExpect(status().is2xxSuccessful());
+    }
+}
+
+
