e927aee1ef5d559b57c6a2bc1fbf7effb4a53ca3
Mateusz Ochel <Mateusz.ohel@gmail.com>
2020-06-15 20:31:51 +0200

    Adding projects through the form

--- src/main/java/pl/app/todoapp/controller/ProjectController.java
+++ src/main/java/pl/app/todoapp/controller/ProjectController.java
@@ -6,15 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.app.todoapp.logic.ProjectService;
 import pl.app.todoapp.model.ProjectStep;
 import pl.app.todoapp.model.projection.ProjectWriteModel;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService service;
+
+    public ProjectController(ProjectService service) {
+        this.service = service;
+    }
+
     @GetMapping
     String showProjects(Model model){
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model){
+        service.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt");
         return "projects";
     }
     @PostMapping(params = "addStep")
