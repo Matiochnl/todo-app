7b48380911d338cf82ae8737a52736144e7c2f80
Mateusz Ochel <Mateusz.ohel@gmail.com>
2020-06-15 20:15:41 +0200

    Add method addProjectStep in ProjectController

--- src/main/java/pl/app/todoapp/controller/ProjectController.java
+++ src/main/java/pl/app/todoapp/controller/ProjectController.java
@@ -3,7 +3,10 @@ package pl.app.todoapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.app.todoapp.model.ProjectStep;
 import pl.app.todoapp.model.projection.ProjectWriteModel;
 
 @Controller
@@ -12,6 +15,11 @@ public class ProjectController {
     @GetMapping
     String showProjects(Model model){
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current){
+        current.getSteps().add(new ProjectStep());
         return "projects";
     }
 }
